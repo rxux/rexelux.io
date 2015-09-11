033b8acb2d32b3da9be85772d15d9e7370a74310
sollenne <rich@sollenne.com>
2015-09-11 11:45:08 -0500

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rexeluxioApp
+ * @description
+ * # rexeluxioApp
+ *
+ * Main module of the application.
+ */
+angular.module('rexeluxioApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
