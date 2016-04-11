fa28e3f331cab1285eb083da84f54ebcd634356f
sollenne <rich@sollenne.com>
2016-04-11 11:29:10 -0500

    restoring grunt file build

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,7 +256,7 @@ module.exports = function (grunt) {
         flow: {
           html: {
             steps: {
-              js: ['concat'],
+              js: ['concat', 'uglifyjs'],
               css: ['cssmin']
             },
             post: {}
@@ -267,7 +267,7 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html', '!<%= yeoman.dist %>/{,*/}{,*/}{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/{,*/}*.html', '!<%= yeoman.dist %>/views/{,*/}{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: [
@@ -466,6 +466,7 @@ module.exports = function (grunt) {
     'copy:dist',
     'cdnify',
     'cssmin',
+    'uglify',
     'filerev',
     'usemin',
     'htmlmin'
